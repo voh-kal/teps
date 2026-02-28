38a384ba7edb019a20d81bd686b04760a16bcb6d
voh-kal <larylivlynnxe@gmail.com>
2026-02-28 19:48:50 +0100

    bcc

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -17,7 +17,7 @@ export default function Navbar() {
         </div>
 
         {/* Center: Search */}
-        <div className="hidden md:flex flex-1 mx-10">
+        {/* <div className="hidden md:flex flex-1 mx-10">
           <div className="relative w-full max-w-md">
             <Search className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400 w-4 h-4" />
             <input
@@ -26,7 +26,7 @@ export default function Navbar() {
               className="w-full pl-10 pr-4 py-2 rounded-[50px] border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-400"
             />
           </div>
-        </div>
+        </div> */}
 
         {/* Right: Links */}
         <div className="hidden md:flex items-center space-x-6">
